f60e29c5b61d522b758eed688fca0034791b31a0
Notebook <rogerioalmeida.gyn@gmail.com>
2020-05-10 17:47:46 -0300

    Adicionando sub

--- Primeira_Impressao.py
+++ Primeira_Impressao.py
@@ -1,4 +1,20 @@
+from unittest import TestCase, main
+
 def soma(x,y):
     return x + y
+def subtracao(x, y):
+    return x - y
 
-print (soma(3,2))
+class Teste(TestCase):
+    def teste_Soma(self):
+        self.assertEqual(soma(5,5),10)
+    def teste_subtracao():
+        self.assertEqual(sub(5,5),0)
+    #def teste_Soma_Errada(self):
+    #    self.assertEqual(soma(5,5),11)
+    
+
+
+if __name__ == "__main__":
+    main()
+    pass
