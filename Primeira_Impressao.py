b8b9463e4717a1ba2c9739a2dbf880f040e8c185
Notebook <rogerioalmeida.gyn@gmail.com>
2020-05-10 18:51:44 -0300

    Add novos arquivos e pastas

--- Primeira_Impressao.py
+++ Primeira_Impressao.py
@@ -1,20 +1,27 @@
 from unittest import TestCase, main
+from abvr import Eh_Par, soma, sub
 
-def soma(x,y):
-    return x + y
-def subtracao(x, y):
-    return x - y
 
 class Teste(TestCase):
     def teste_Soma(self):
         self.assertEqual(soma(5,5),10)
-    def teste_subtracao():
-        self.assertEqual(sub(5,5),0)
+
+    def Teste_Eh_Par(self):
+        self.assertEqual(Eh_Par(3), True)
+
+    #def Teste_Eh_Da_String(self):
+    #    self.assertEqual(Eh_Par("string"),False)
+    
+    def teste_Sub(self):
+        self.assertEqual(sub(10,10),0)
+
+
     #def teste_Soma_Errada(self):
     #    self.assertEqual(soma(5,5),11)
     
 
 
 if __name__ == "__main__":
+    Eh_Par(3)
     main()
     pass
---
+++ abvr.py
@@ -0,0 +1,13 @@
+def soma(x,y):
+    return x + y
+
+"""
+Função que verifica se eh par.
+arg:
+    val: tipo inteira
+"""
+def Eh_Par(val:int ) -> bool:
+    return True if val % 2 ==0 else False
+
+def sub(x, y):
+    return x - y
